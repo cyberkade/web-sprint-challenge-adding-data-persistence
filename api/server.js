f1069fd733e03c7a1857837733d62a7e51a46442
Kade Griffith <kadeg95@gmail.com>
2021-10-08 10:05:30 -0600

    installed all needed dependencies for this project, created a server by connecting index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-// build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+    });
+  });
+
+module.exports = server
+
